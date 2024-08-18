2188f3cb783b4c4da366a58fc8220096a4178f74
lada <lada.batalova@gmail.com>
2024-08-18 20:47:37 +0300

    Addaptive design

--- src/widgets/Header/ui/Header/index.tsx
+++ src/widgets/Header/ui/Header/index.tsx
@@ -7,11 +7,18 @@ import { useTypedSelector } from "../../../../shared/lib/hooks/useTypedSelector"
 import MyModal from "../../../../shared/ui/modal/MyModal";
 import { AuthModalForm } from "../../../../features/Auth";
 import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import './header.scss';
 
 export const Header = () => {
     const {isAuth} = useTypedSelector(state => state.user);
     const [modal, setModal] = useState(false)
+    const [sidebarActive, setSidebarActive] = useState<boolean>(false);
+    useEffect((): void => {
+        if (sidebarActive) document.body.classList.add('show-nav')
+        else document.body.classList.remove('show-nav')
+    }, [sidebarActive])
+
 
     //check if user in sign-in, sign-up or reset pages to hide button 'sign-in' in this way
     const location = useLocation();
@@ -25,7 +32,7 @@ export const Header = () => {
                 <div className="header__inner">
                     <Logo />
                     <ul className="header__list">
-                        <div className="header__item">
+                        <div className="header__item header__item--tel">
                             <a className="header__link" href="tel:+7800*******">+7-800-***-**-**</a>
                         </div>
                         <li className="header__item">
@@ -49,6 +56,14 @@ export const Header = () => {
                                         <AuthModalForm active={modal} setActive={setModal}/>
                                     </MyModal>
                                 </>}
+                        </li>
+                        <li className="header__item">
+                            <button className='header__burger'
+                                type="button"
+                                onClick={(): void => setSidebarActive(!sidebarActive)
+                                }>
+                                <span>Menu</span>
+                            </button>
                         </li>
                     </ul>
                 </div>
